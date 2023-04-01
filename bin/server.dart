6700c9926bbf3b44db5e3d9b4006a708c6955515
Plague Fox <plugfox@gmail.com>
2023-04-01 17:38:32 +0400

    Add logging to the server to help debug issues

--- bin/server.dart
+++ bin/server.dart
@@ -10,15 +10,28 @@ import 'package:rxdart/rxdart.dart';
 
 @pragma('vm:entry-point')
 void main([List<String>? args]) => runZonedGuarded<Future<void>>(() async {
+      fine('Starting server...');
       final cpuCount = math.max(io.Platform.numberOfProcessors, 2);
-      final dbFile = await io.Directory.current
-          .list(recursive: true)
-          .whereType<io.File>()
-          .where((e) => e.path.endsWith('.sqlite'))
-          .first;
-      final db = Database.lazy(
-        file: io.File(dbFile.absolute.path),
-      );
+      final Database db;
+      try {
+        final dbFile = await io.Directory.current
+            .list(recursive: true)
+            .whereType<io.File>()
+            .where((e) => e.path.endsWith('.sqlite'))
+            .first;
+        fine('Using database: ${dbFile.absolute.path}');
+        db = Database.lazy(
+          file: io.File(dbFile.absolute.path),
+        );
+      } on StateError {
+        severe('Database not found, try to generate it first.');
+        rethrow;
+      } on Object {
+        severe('Problem with database initialization.');
+        rethrow;
+      }
+      const httpPort = 8080;
+      fine('Starting $cpuCount server(s) on port $httpPort');
       for (var i = 1; i <= cpuCount; i++) {
         final receivePort = ReceivePort();
         // ignore: unused_local_variable
@@ -38,6 +51,7 @@ void main([List<String>? args]) => runZonedGuarded<Future<void>>(() async {
           cancelOnError: false,
         );
       }
+      config('Server started on port $httpPort');
     }, (error, stackTrace) {
       severe('Error: $error\n\n$stackTrace');
       io.exit(2);
